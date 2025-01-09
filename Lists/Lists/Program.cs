189a6b0b44cc3001e05bfb1e511a28fae71bebd1
kareem <120470069+kareem221215@users.noreply.github.com>
2025-01-09 05:33:29 +0300

    Add files via upload

---
+++ Lists/Lists/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List <string> names = new List<string> { "Alice", "Bob", "Charlie", "Diana", "Edward", "Fiona", "George", "Hannah", "Irene", "Jack" };
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine(i+1+"-"+names[i]);
+            }
+        }
+    }
+}
