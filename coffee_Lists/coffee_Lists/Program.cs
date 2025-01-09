ba17d81fd5be737754cc026e935680df528c5cf5
kareem <120470069+kareem221215@users.noreply.github.com>
2025-01-09 05:47:15 +0300

    Add files via upload

---
+++ coffee_Lists/coffee_Lists/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace coffee_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Coffee = new List<string>();
+            string names;
+
+            Console.WriteLine("please enter 5 coffee names");
+            for (int i = 0; i < 5; i++)
+            {
+                names = Console.ReadLine();
+                Coffee.Add(names);
+            }
+            Console.WriteLine("here are your coffee names");
+            for (int i = 0;i < 5; i++)
+            {
+                Console.WriteLine(i+1+"-"+Coffee[i]);
+            }
+        }
+    }
+}
