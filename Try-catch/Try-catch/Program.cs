aba8941490aff8714ba951381f77c4d26f54ec37
kareem <120470069+kareem221215@users.noreply.github.com>
2025-01-13 01:43:11 +0300

    Add files via upload

---
+++ Try-catch/Try-catch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try_catch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("choose a number");
+
+            try
+            {
+
+                string input =  Console.ReadLine();
+
+                int number = int.Parse(input);
+                int square =  number * number;
+                Console.WriteLine($"the square of the  number  is {square}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("wrong input please enter a number");
+            }
+        }
+    }
+}
